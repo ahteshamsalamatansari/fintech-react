48cc69b2f9fcb8d753fe2ceeaea91c3926618944
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-01-26 00:17:04 +0530

    second button added

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import {features} from '../constants/index';
+import styles , { layout } from '../style';
+import Button from './Button';
+const Business = () => 
+   (
+    <section id='features' className={layout.section}>
+       <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>You do the business,<br className='hidden sm:block'/> we’ll handle the money.</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}> With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market. </p>
+          <Button styles="mt-10"/>
+        </div>
 
-const Business = () => {
-  return (
-    <div>Business</div>
+    </section>
   )
-}
+
 
 export default Business
