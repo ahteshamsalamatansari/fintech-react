0162c906226f431c06d2a32b1657dba6d7574461
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-01-26 12:42:17 +0530

    Footer Started

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div>Footer</div>
+import styles from "../style"
+import {logo} from "../assets"
+import {footerLinks, socialMedia} from "../constants" 
+const Footer = () => 
+   (
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        
+      </div>
+    </section>
   )
-}
+
 
 export default Footer
